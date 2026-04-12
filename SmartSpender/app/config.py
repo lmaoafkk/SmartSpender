c3574249ac4d4f3a833c5043af064665017a35d3
lmaoafkk <ramsundarrondell@gmail.com>
2026-04-11 22:31:50 -0400

    Add render.yaml to use pip and update port config for Render

--- SmartSpender/app/config.py
+++ SmartSpender/app/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
+import os
 
 @lru_cache
 def get_settings():
@@ -12,7 +13,7 @@ class Settings(BaseSettings):
     jwt_algorithm: str="HS256"
     jwt_access_token_expires:int=30
     app_host: str="0.0.0.0"
-    app_port: int=8000
+    app_port: int=int(os.getenv("PORT", 8000))
     db_pool_size:int=10
     db_additional_overflow:int=10
     db_pool_timeout:int=10
